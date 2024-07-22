3c04c9d8783c41d1062119bd3680988303a374b0
Gustaf <gustaf.j.dahl@gmail.com>
2024-07-22 18:01:00 +0200

    does this work now=

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+
+export default {
+  input: './dist/index.js',
+  output: {
+    dir: './dist',
+    entryFileNames: '[name].cjs',
+    format: 'cjs',
+  },
+  plugins: [nodeResolve(), commonjs(), json()],
+};
