4807054169c6f3db227f8ffa383543a942461100
Gustaf <gustaf.j.dahl@gmail.com>
2024-07-22 21:48:29 +0200

    Might work

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,9 @@
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-
 export default {
   input: './dist/index.js',
   output: {
-    dir: 'dist/',
+    dir: 'dist',
     format: 'cjs',
     entryFileNames: '[name].cjs',
-    // inlineDynamicImports: true, // Inlines dynamic imports if you prefer a single output file
     preserveModules: true,
   },
-  // plugins: [nodeResolve(), commonjs(), json(), typescript()],
 };
