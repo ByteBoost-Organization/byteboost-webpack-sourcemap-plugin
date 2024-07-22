256e3a8371461676b8265295ae7d6094be11fc94
Gustaf <gustaf.j.dahl@gmail.com>
2024-07-22 18:21:38 +0200

    this might work

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: './dist/index.js',
+  input: 'src/index.ts',
   output: {
-    dir: './dist',
-    entryFileNames: '[name].cjs',
-    format: 'cjs',
+    dir: 'dist',
+    format: 'esm',
   },
-  plugins: [nodeResolve(), commonjs(), json()],
+  plugins: [nodeResolve(), commonjs(), json(), typescript()],
 };
