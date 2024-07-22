74582e0db22ba07ffd0f302fd078caf93bcc3c11
Gustaf <gustaf.j.dahl@gmail.com>
2024-07-22 21:20:50 +0200

    forgot to run rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,7 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import { format } from 'path';
 
 export default {
   input: './dist/index.js',
